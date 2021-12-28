5f4f1a5d34636b33542098dfd91b4deb9d490ebe
Sebastian Schmidt <isibboi@gmail.com>
2021-12-28 15:15:41 +0200

    Document existing vocabulary schema.

--- backend/rvoc-backend/src/database/model/vocabulary/mod.rs
+++ backend/rvoc-backend/src/database/model/vocabulary/mod.rs
@@ -3,19 +3,24 @@ use wither::bson::oid::ObjectId;
 use wither::mongodb::bson::doc;
 use wither::Model;
 
+/// A language.
 #[derive(Debug, Model, Serialize, Deserialize)]
 #[model(index(keys = r#"doc!{"name": 1}"#, options = r#"doc!{"unique": true}"#))]
 pub struct Language {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
+    /// The name of the language.
     pub name: String,
 }
 
+/// A single word in a language.
 #[derive(Debug, Model, Serialize, Deserialize)]
 #[model(index(keys = r#"doc!{"language_name": 1}"#))]
 pub struct Word {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
+    /// The name of the language this word belongs to.
     pub language_name: String,
+    /// The word's textual representation.
     pub word: String,
 }
