6a19759dab47453f67551dd33e3dc6f55b1c37a8
Sebastian Schmidt <isibboi@gmail.com>
2021-12-28 15:08:25 +0200

    Move vocabulary related schemas into submodule.

--- backend/rvoc-backend/src/database/model/mod.rs
+++ backend/rvoc-backend/src/database/model/mod.rs
@@ -1,27 +1,10 @@
 use crate::RVocResult;
 use log::info;
-use serde::{Deserialize, Serialize};
-use wither::bson::oid::ObjectId;
-use wither::mongodb::bson::doc;
 use wither::mongodb::Database;
 use wither::Model;
+use crate::database::model::vocabulary::{Language, Word};
 
-#[derive(Debug, Model, Serialize, Deserialize)]
-#[model(index(keys = r#"doc!{"name": 1}"#, options = r#"doc!{"unique": true}"#))]
-pub struct Language {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    pub name: String,
-}
-
-#[derive(Debug, Model, Serialize, Deserialize)]
-#[model(index(keys = r#"doc!{"language_name": 1}"#))]
-pub struct Word {
-    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<ObjectId>,
-    pub language_name: String,
-    pub word: String,
-}
+pub mod vocabulary;
 
 pub async fn sync_model(database: &Database) -> RVocResult<()> {
     info!("Syncing database model...");
