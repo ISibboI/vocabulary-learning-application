5744954f84926d4900ca52248e7a896df92a1492
Sebastian Schmidt <isibboi@gmail.com>
2023-07-18 13:28:27 +0300

    Setup global project build.

--- flake.nix
+++ flake.nix
@@ -26,9 +26,9 @@
           pkgs = import nixpkgs {
             inherit system overlays;
           };
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rvoc-backend/rust-toolchain.toml;
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-          src = craneLib.cleanCargoSource ./rvoc-backend;
+          src = craneLib.cleanCargoSource ./.;
           nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
           buildInputs = with pkgs; [rustToolchain openssl];
           commonArgs = {
