3819cd0d27368a9e200cc09354368499414b73cd
Sebastian Schmidt <isibboi@gmail.com>
2023-08-11 11:57:29 +0300

    Change timezone to timezone with time stamp.

--- backend/rvoc-backend/migrations/2023-08-08-084449_create_job_queue/up.sql
+++ backend/rvoc-backend/migrations/2023-08-08-084449_create_job_queue/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE job_queue (
-	scheduled_execution_time TIMESTAMP NOT NULL,
+	scheduled_execution_time TIMESTAMP WITH TIME ZONE NOT NULL,
 	name TEXT NOT NULL,
 	in_progress BOOLEAN NOT NULL DEFAULT false,
 	PRIMARY KEY (scheduled_execution_time, name)
