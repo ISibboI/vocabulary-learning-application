a4121e5fcb44b92f4185e9d2088ea26fbf9b93e5
Sebastian Schmidt <isibboi@gmail.com>
2023-08-11 09:11:06 +0300

    Give job queue primary key (time, name).
    This way, conflicts between jobs with different names can be avoided.

--- backend/rvoc-backend/migrations/2023-08-08-084449_create_job_queue/up.sql
+++ backend/rvoc-backend/migrations/2023-08-08-084449_create_job_queue/up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE job_queue (
-	scheduled_execution_time TIMESTAMP PRIMARY KEY,
+	scheduled_execution_time TIMESTAMP NOT NULL,
 	name TEXT NOT NULL,
-	in_progress BOOLEAN NOT NULL DEFAULT false
+	in_progress BOOLEAN NOT NULL DEFAULT false,
+	PRIMARY KEY (scheduled_execution_time, name)
 );
