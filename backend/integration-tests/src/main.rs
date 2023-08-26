d96daf15cecf1295b6f1ba7d02062cf6db583dff
Sebastian Schmidt <isibboi@gmail.com>
2023-08-26 16:39:23 +0300

    Print complete response on integration test error.

--- backend/integration-tests/src/main.rs
+++ backend/integration-tests/src/main.rs
@@ -42,6 +42,7 @@ async fn test_user_account_creation(client: &HttpClient) {
     assert_eq!(
         response.status(),
         StatusCode::OK,
-        "unexpected response: {response:?}"
+        "unexpected response:\n{:?}\n",
+        std::str::from_utf8(response.bytes().await.unwrap().as_ref()),
     );
 }
