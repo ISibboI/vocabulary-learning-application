97c075a031cc9c5244d9987865670f5d61d45df9
Sebastian Schmidt <isibboi@gmail.com>
2023-10-14 18:39:03 +0300

    Justify using read-committed mode in comment.

--- backend/rvoc-backend/src/job_queue/jobs/delete_expired_sessions.rs
+++ backend/rvoc-backend/src/job_queue/jobs/delete_expired_sessions.rs
@@ -8,6 +8,9 @@ pub async fn delete_expired_sessions(
     database_connection_pool: &RVocAsyncDatabaseConnectionPool,
     configuration: &Configuration,
 ) -> RVocResult<()> {
+    // We execute this in read-committed mode to hopefully make it never fail,
+    // since it potentially touches a lot of rows. Rather, if a session gets
+    // deleted while another transaction updates it, we don't care and delete it anyways.
     database_connection_pool
         .execute_read_committed_transaction::<_, RVocError>(
             |database_connection| {
